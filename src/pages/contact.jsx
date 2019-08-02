ee0cf12b5b9cdbf1501d992154af64e0a60770d2
Hugo <hugo@unscuzzy.com>
2019-08-02 13:54:17 +0200

    test form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -46,6 +46,17 @@ const ContactPage = ({ data }) => {
             </Tween>
           )}
         </VisibilitySensor>
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </Container>
     </Layout>
   )
